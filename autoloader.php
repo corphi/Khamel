f2570fc579018762cd86573c828dbd68b46321fd
Philipp Cordes <pc@irgendware.net>
2013-01-04 15:31:30 +0100

    Streamlined the autoloader.

--- autoloader.php
+++ autoloader.php
@@ -4,11 +4,16 @@ namespace Khamel;
 
 
 
+/**
+ * Try to load a Khamel class.
+ * @param string $name
+ */
 function autoloader($name)
 {
 	if (substr($name, 0, 7) !== 'Khamel\\') {
 		return false;
 	}
-	return include __DIR__ . '/' . str_replace('\\', '/', substr($name, 7)) . '.php';
+	return include(__DIR__ . '/' . str_replace('\\', '/', substr($name, 7)) . '.php');
 }
+
 spl_autoload_register('Khamel\\autoloader');
